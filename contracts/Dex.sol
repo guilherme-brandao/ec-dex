2f794e824496ba87fab6130b4b8755f075ef420e
Guilherme Brandão <guilherme1237@gmail.com>
2021-04-19 21:50:32 -0300

    feat: Limit order

--- contracts/Dex.sol
+++ contracts/Dex.sol
@@ -5,7 +5,8 @@ import "./Wallet.sol";
 
 contract Dex is Wallet {
 
-    //Tipo um booleano para indicar o tipo de ordem
+    using SafeMath for uint256;
+
     enum Side {
         BUY,
         SELL
@@ -16,19 +17,71 @@ contract Dex is Wallet {
         address trader;
         Side buyOrder;
         bytes32 ticker;
-        uint amountl;
+        uint amount;
         uint price;
     }
+
+    uint public nextOrderId = 0;
     
-    //Esse mapping cria um ordem book para BUY e outro para SELL para cada ticker
     mapping(bytes32 => mapping(uint => Order[])) orderBook;
 
     function getOrderBook(bytes32 ticker, Side side) view public returns(Order[] memory) {
         return orderBook[ticker][uint(side)];
     }
 
-    // function createLimitOrder() {
+    function createLimitOrder(Side side, bytes32 ticker, uint amount, uint price) public {
+        if(side == Side.BUY) {
+            require(balances[msg.sender][bytes32("ETH")] >= amount.mul(price), "You don't have this much ETH to buy these tokens!");
+        } else if(side == Side.SELL) {
+            require(balances[msg.sender][ticker] >= amount, "You dont have this much to sell!");
+        }
 
-    // }
+        Order[] storage orders = orderBook[ticker][uint(side)];
+        
+        orders.push(
+            Order(nextOrderId, msg.sender, side, ticker, amount, price)
+        );
+
+        uint i = orders.length > 0 ?  orders.length - 1 : 0;
+
+        if(side == Side.BUY) {
+
+            while(i > 0) {
+                if(orders[i - 1].price > orders[i].price) {
+                    break;
+                }
+
+                Order memory orderToMove = orders[i - 1];
+                orders[i - 1] = orders[i];
+                orders[i] = orderToMove;
+                i--;
+            }
+        } else if(side == Side.SELL) {
+
+            while(i > 0) {
+                if(orders[i - 1].price < orders[i].price) {
+                    break;
+                }
+
+                Order memory orderToMove = orders[i - 1];
+                orders[i - 1] = orders[i];
+                orders[i] = orderToMove;
+                i--;
+            }
+        }
+        nextOrderId++;
+
+        return;
+    }
+
+    function createMarketOrder(Side side, bytes32 ticker, uint amount) {
+        if(side == Side.BUY) {
+            require(balances[msg.sender][bytes32("ETH")] >= amount.mul(price), "You don't have this much ETH to buy these tokens!");
+        } else if(side == Side.SELL) {
+            require(balances[msg.sender][ticker] >= amount, "You dont have this much to sell!");
+        }
+        
+
+    }
 
 }
